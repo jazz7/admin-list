5775c4b42ca50220f849417d8110e2be40d212ad
Jaseem AR <jaseemjazzz@gmail.com>
2017-11-13 22:55:56 +0530

    Add dummy data

---
+++ data.js
@@ -0,0 +1,26 @@
+var data = [
+  {
+    name: "Alex Ferguson",
+    email: "Chaim_McDermott@dana.io",
+    address: {
+      street: "Dayna Park",
+      suite: "Suite 449",
+      city: "Bartholomebury",
+      zipcode: "76495-3109"
+    },
+    phone: "(775)976-6794 x41206",
+    website: "conrad.com"
+  },
+  {
+    name: "Alex Ferguson",
+    email: "Chaim_McDermott@dana.io",
+    address: {
+      street: "Dayna Park",
+      suite: "Suite 449",
+      city: "Bartholomebury",
+      zipcode: "76495-3109"
+    },
+    phone: "(775)976-6794 x41206",
+    website: "conrad.com"
+  }
+]
